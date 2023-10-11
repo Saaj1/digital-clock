56261f3ff029421c228459956589cf7500f09f3f
saajida mathen <mathensaajida@gmail.com>
2023-10-11 12:08:17 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import DigitalClock from './Components/DigitalClock'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <DigitalClock />
+      </div>
+    </>
+  )
+}
+
+export default App
